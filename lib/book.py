a96b2b30c9367879a59c5e5dd02453983b2482a4
Kerry Guarino <kerry.guarino@gmail.com>
2023-09-10 07:27:42 -0400

    completed shoe class object

--- lib/book.py
+++ lib/book.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
 
 class Book:
-    def __init__(self, title ="title", page_count = 10, turn_page = "yes"):
+    def __init__(self, title ="title", page_count = 10):
         self.title = title
         self.page_count = page_count
-        self.turn_page = turn_page
     
     def get_title(self):
         return self._title
@@ -14,6 +13,9 @@ class Book:
             self._title = title
         else:
             print("not a real title")
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
 
     title = property(get_title, set_title)
     
@@ -28,11 +30,6 @@ class Book:
 
     page_count = property(get_page_count, set_page_count)
 
-    def get_turn_page(self):
-        return self._turn_page
-
-    def set_turn_page(self, turn_page):
-        self._turn_page = turn_page
-        print("Flipping the page...wow, you read fast!")
     
-    turn_page = property(get_turn_page, set_turn_page)
+    
+    
